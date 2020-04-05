c6d132fdac76f1e06f00aaf7df295b8c7a212042
khh2hoya <khh2hoya@naver.com>
2020-04-06 02:16:34 +0900

    getter, setter 사용. 필드를 만들고 나서  eclipse 에서 [Source] - [Generate Getters and Setters] 하면 자동으로 만들어준다.

--- chap06/src/sec06/exam04/package2/C.java
+++ chap06/src/sec06/exam04/package2/C.java
@@ -4,6 +4,6 @@ import sec06.exam04.package1.A;
 
 public class C {
 	A a1 = new A(true);
-	A a2 = new A(1);
-	A a3 = new A("문자열");
+	//A a2 = new A(1); 		//compile error
+	//A a3 = new A("문자열");	//compile error
 }
