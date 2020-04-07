411783ccc54d452e2e13c66b937ae48536318e9d
khh2hoya <khh2hoya@gmail.com>
2020-04-08 04:23:41 +0900

    다형성에 대해 공부함.

--- chap07/src/sec01/exam06/Car.java
+++ chap07/src/sec01/exam06/Car.java
@@ -1,5 +1,20 @@
 package sec01.exam06;
 
 public class Car {
+	//필드
+	public int speed;
+	
+	//생성자
+	
+	//메소드
+	public void speedUp() {
+		speed += 1;
+	}
+	
+	//
+	public final void stop() {
+		System.out.println("Stop");
+		speed = 0;
+	}
 
 }
--- chap07/src/sec01/exam06/SportsCar.java
+++ chap07/src/sec01/exam06/SportsCar.java
@@ -1,5 +1,14 @@
 package sec01.exam06;
 
-public class SportsCar {
+public class SportsCar extends Car{
+	@Override
+	public void speedUp() {
+		// TODO Auto-generated method stub
+		speed += 10;
+	}
+	
+	//stop 은 final 로 선언 되서 재정의 할수 없다.
+//	public void stop() {
+//	}
 
 }
