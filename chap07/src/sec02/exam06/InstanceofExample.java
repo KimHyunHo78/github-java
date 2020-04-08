e55754319510c01799fabd60923b836428a8e82b
khh2hoya <khh2hoya@gmail.com>
2020-04-09 01:18:04 +0900

    추상화 클래스  - 다형성을 구현 할수 있다. 인터페이스  - 추상화와 비슷 하지만 좀 다름.  - 구현되 객체를 인터페이스를 통해 다형성을 구현 할수 있다.

--- chap07/src/sec02/exam06/InstanceofExample.java
+++ chap07/src/sec02/exam06/InstanceofExample.java
@@ -1,10 +1,34 @@
 package sec02.exam06;
 
+
+
 public class InstanceofExample {
 
+	public static void method1(Parent parent) {
+		if(parent instanceof Child) {
+			Child child = (Child)parent;
+			System.out.println("method1 - Child로 변환 성공.");
+		}else {
+			System.out.println("method1 - Child로 변환도지 않음.");
+		}
+	}
+	
+	public static void method2(Parent parent) {
+		Child child = (Child)parent;
+		System.out.println("method1 - Child로 변환 성공.");
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Parent parentA = new Child();
+		method1(parentA);
+		method2(parentA);
+		
+		Parent parentB = new Parent();
+		method1(parentB);
+		method2(parentB);
 	}
 
 }
+
+// instanceof 연산자로  부모가 instance 가  작식으로 만들어 졌는지 확인 한다.
